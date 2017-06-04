b03016ead641160f53f13f00c55d54f325fd08c1
Etienne Laval <etienne.laval.moi@gmail.com>
2017-06-04 23:46:45 +0200

    Clustering Ok on dirait

---
+++ src/Algos/Clusteriser.java
@@ -0,0 +1,11 @@
+package Algos;
+
+import GraphTools.Graph;
+
+/**
+ * Created by etien on 04/06/2017.
+ */
+public class Clusterify {
+    protected int maxBeetwinnessAllowed;
+    protected Graph
+}
